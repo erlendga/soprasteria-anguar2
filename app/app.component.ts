f956aa77deeda2ae6c6286f8dd94e70ab70961cd
Nitharshaan Thevarajah <nitharshaan@gmail.com>
2016-10-05 12:45:22 +0200

    endret på fnutter

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'sopra-steria',
-  template: '<h1>Hello, {{title}}!</h1>'
+  template: `
+    <h1>Hello, {{title}}!</h1>
+  `
 })
 export class AppComponent {
   title = 'Online';
